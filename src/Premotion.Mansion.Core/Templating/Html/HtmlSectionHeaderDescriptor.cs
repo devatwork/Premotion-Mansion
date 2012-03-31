e8a95855d7e038ae6e62c8a0fa028c2eef671b7c
Bert Willems <b.willems@premotion.nl>
2012-03-31 14:39:21 +0200

    * descriptor

--- src/Premotion.Mansion.Core/Templating/Html/HtmlSectionHeaderDescriptor.cs
+++ src/Premotion.Mansion.Core/Templating/Html/HtmlSectionHeaderDescriptor.cs
@@ -6,7 +6,7 @@ namespace Premotion.Mansion.Core.Templating.Html
 	/// <summary>
 	/// Defines the descriptor for template headers.
 	/// </summary>
-	[Named(typeof (IDescriptor), "http://schemas.premotion.nl/mansion/1.0/template.xsd", "section")]
+	[Named(typeof (HtmlSectionHeaderDescriptor), "http://schemas.premotion.nl/mansion/1.0/template.xsd", "section")]
 	public class HtmlSectionHeaderDescriptor : Descriptor
 	{
 	}
