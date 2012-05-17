9d252280ce98c1752a0b5b8838f0cd2854c662ec
Bert Willems <b.willems@premotion.nl>
2012-05-17 15:05:29 +0200

    symbol packages

---
+++ src/package.cmd
@@ -0,0 +1,15 @@
+.nuget\NuGet.exe update -self
+
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Premotion.Mansion.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
+.nuget\NuGet.exe pack Premotion.Mansion.Amazon\Premotion.Mansion.Amazon.nuspec
+.nuget\NuGet.exe pack Premotion.Mansion.Core\Premotion.Mansion.Core.nuspec
+.nuget\NuGet.exe pack Premotion.Mansion.Repository.SqlServer\Premotion.Mansion.Repository.SqlServer.nuspec
+.nuget\NuGet.exe pack Premotion.Mansion.Web\Premotion.Mansion.Web.nuspec
+.nuget\NuGet.exe pack Premotion.Mansion.Web.Portal\Premotion.Mansion.Web.Portal.nuspec
+
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Premotion.Mansion.sln /t:Clean,Rebuild /p:Configuration=Debug /fileLogger
+.nuget\NuGet.exe pack Premotion.Mansion.Amazon\Premotion.Mansion.Amazon.symbols.nuspec -Symbols
+.nuget\NuGet.exe pack Premotion.Mansion.Core\Premotion.Mansion.Core.symbols.nuspec -Symbols
+.nuget\NuGet.exe pack Premotion.Mansion.Repository.SqlServer\Premotion.Mansion.Repository.SqlServer.symbols.nuspec -Symbols
+.nuget\NuGet.exe pack Premotion.Mansion.Web\Premotion.Mansion.Web.symbols.nuspec -Symbols
+.nuget\NuGet.exe pack Premotion.Mansion.Web.Portal\Premotion.Mansion.Web.Portal.symbols.nuspec -Symbols
