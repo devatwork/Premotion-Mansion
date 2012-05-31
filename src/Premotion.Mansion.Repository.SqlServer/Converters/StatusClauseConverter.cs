49e0ef305758bddafbc855b84279254990f0c472
Bert Willems <b.willems@premotion.nl>
2012-05-31 21:52:10 +0200

    + use web server time to query instead of database server time

--- src/Premotion.Mansion.Repository.SqlServer/Converters/StatusClauseConverter.cs
+++ src/Premotion.Mansion.Repository.SqlServer/Converters/StatusClauseConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 using System.Text;
 using Premotion.Mansion.Core;
 using Premotion.Mansion.Core.Data;
@@ -29,18 +30,21 @@ namespace Premotion.Mansion.Repository.SqlServer.Converters
 
 			var buffer = new StringBuilder("( ");
 
+			// create the parameter for now, because we do not trust the database server time
+			var nowParameterName = command.AddParameter(DateTime.Now);
+
 			// check statusses
 			if ((clause.Status & NodeStatus.Draft) == NodeStatus.Draft)
 				buffer.AppendFormat("([{0}].[approved] = 0 AND [{0}].[archived] = 0)", schema.RootTable.Name);
 
 			if ((clause.Status & NodeStatus.Staged) == NodeStatus.Staged)
-				buffer.AppendFormat("{1}([{0}].[approved] = 1 AND [{0}].[publicationDate] > GETDATE() AND [{0}].[archived] = 0)", schema.RootTable.Name, buffer.Length > 2 ? " OR " : string.Empty);
+				buffer.AppendFormat("{1}([{0}].[approved] = 1 AND [{0}].[publicationDate] > @{2} AND [{0}].[archived] = 0)", schema.RootTable.Name, buffer.Length > 2 ? " OR " : string.Empty, nowParameterName);
 
 			if ((clause.Status & NodeStatus.Published) == NodeStatus.Published)
-				buffer.AppendFormat("{1}([{0}].[approved] = 1 AND [{0}].[publicationDate] <= GETDATE() AND [{0}].[expirationDate] >= GETDATE() AND [{0}].[archived] = 0)", schema.RootTable.Name, buffer.Length > 2 ? " OR " : string.Empty);
+				buffer.AppendFormat("{1}([{0}].[approved] = 1 AND [{0}].[publicationDate] <= @{2} AND [{0}].[expirationDate] >= @{2} AND [{0}].[archived] = 0)", schema.RootTable.Name, buffer.Length > 2 ? " OR " : string.Empty, nowParameterName);
 
 			if ((clause.Status & NodeStatus.Expired) == NodeStatus.Expired)
-				buffer.AppendFormat("{1}([{0}].[approved] = 1 AND [{0}].[expirationDate] < GETDATE() AND [{0}].[archived] = 0)", schema.RootTable.Name, buffer.Length > 2 ? " OR " : string.Empty);
+				buffer.AppendFormat("{1}([{0}].[approved] = 1 AND [{0}].[expirationDate] < @{2} AND [{0}].[archived] = 0)", schema.RootTable.Name, buffer.Length > 2 ? " OR " : string.Empty, nowParameterName);
 
 			if ((clause.Status & NodeStatus.Archived) == NodeStatus.Archived)
 				buffer.AppendFormat("{1} [{0}].[archived] = 1", schema.RootTable.Name, buffer.Length > 2 ? " OR " : string.Empty);
