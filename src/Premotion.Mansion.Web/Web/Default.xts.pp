e8d2ad30f1d237ebe3b849bedcab4ed3054de113
Bert Willems <b.willems@premotion.nl>
2012-06-02 21:09:21 +0200

    added a default.xts transform

---
+++ src/Premotion.Mansion.Web/Web/Default.xts.pp
@@ -0,0 +1,7 @@
+﻿<?xml version="1.0" encoding="utf-8" ?>
+<mansion xmlns="http://schemas.premotion.nl/mansion/1.0/stl.xsd">
+
+	<!-- here be dragons, add your fancy application code here -->
+	<!-- a word of advice though: use the dispatcher module    -->
+
+</mansion>
