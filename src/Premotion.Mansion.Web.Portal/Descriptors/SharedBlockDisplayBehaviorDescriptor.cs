4f3983d5bb562097aca978909eaa8d40236e8619
Bert Willems <b.willems@premotion.nl>
2012-06-03 12:35:27 +0200

    #16 Create infra for shared blocks

---
+++ src/Premotion.Mansion.Web.Portal/Descriptors/SharedBlockDisplayBehaviorDescriptor.cs
@@ -0,0 +1,63 @@
+using System;
+using Premotion.Mansion.Core;
+using Premotion.Mansion.Core.Collections;
+using Premotion.Mansion.Core.Types;
+using Premotion.Mansion.Web.Portal.Service;
+
+namespace Premotion.Mansion.Web.Portal.Descriptors
+{
+	/// <summary>
+	/// Implements the behavior for shared blocks displays.
+	/// </summary>
+	[TypeDescriptor(Constants.DescriptorNamespaceUri, "sharedBlockDisplayBehavior")]
+	public class SharedBlockDisplayBehaviorDescriptor : BlockBehaviorDescriptor
+	{
+		#region Constructors
+		/// <summary>
+		/// </summary>
+		/// <param name="portalService">The <see cref="IPortalService"/>.</param>
+		public SharedBlockDisplayBehaviorDescriptor(IPortalService portalService)
+		{
+			// validate arguments
+			if (portalService == null)
+				throw new ArgumentNullException("portalService");
+
+			// set values
+			this.portalService = portalService;
+		}
+		#endregion
+		#region Overrides of BlockBehaviorDescriptor
+		/// <summary>
+		/// Renders the specified <paramref name="blockProperties"/> to the output pipe.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="blockProperties">The <see cref="IPropertyBag"/> of the block which to render.</param>
+		/// <param name="targetField">The name of the field to which to render.</param>
+		protected override void DoRender(IMansionContext context, IPropertyBag blockProperties, string targetField)
+		{
+			// first retrieve the block node to display
+			Guid displayedBlockGuid;
+			if (!blockProperties.TryGet(context, "blockGuid", out displayedBlockGuid))
+				throw new InvalidOperationException("Block guid not found for shared block display");
+			var displayedBlockNode = context.Repository.RetrieveSingle(context, new PropertyBag
+			                                                                    {
+			                                                                    	{"guid", displayedBlockGuid}
+			                                                                    });
+			if (displayedBlockNode == null)
+				throw new InvalidOperationException(string.Format("Could not find block with guid '{0}'", displayedBlockGuid));
+
+			// second, merge the two block properties together
+			var mergedBlockProperties = new PropertyBag();
+			mergedBlockProperties.Merge(blockProperties);
+			mergedBlockProperties.Merge(displayedBlockNode);
+			mergedBlockProperties.Set("id", blockProperties.Get<int>(context, "id"));
+
+			// finally re-render the combined block using the portal service
+			portalService.RenderBlock(context, mergedBlockProperties, targetField);
+		}
+		#endregion
+		#region Private Fields
+		private readonly IPortalService portalService;
+		#endregion
+	}
+}
