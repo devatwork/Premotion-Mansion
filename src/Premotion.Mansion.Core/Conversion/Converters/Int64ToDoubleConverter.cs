18974e93449795f35b2858e46eabb50e5ae61f3b
Bert Willems <b.willems@premotion.nl>
2012-06-23 17:57:53 +0200

    + added the long to double converter

---
+++ src/Premotion.Mansion.Core/Conversion/Converters/Int64ToDoubleConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Premotion.Mansion.Core.Conversion.Converters
+{
+	/// <summary>
+	/// Handles the conversion from <see cref="long"/> to <see cref="double"/>.
+	/// </summary>
+	public class Int64ToDoubleConverter : ConverterBase<Int64, double>
+	{
+		#region Overrides of ConverterBase<Int64,Int32>
+		/// <summary>
+		/// Converts the object to <see cref="IConverter.TargetType"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="source">The input value.</param>
+		/// <param name="sourceType">The actual type of the source.</param>
+		/// <returns>Returns the converted value.</returns>
+		protected override double DoConvert(IMansionContext context, Int64 source, Type sourceType)
+		{
+			return source;
+		}
+		/// <summary>
+		/// Converts the object to <see cref="IConverter.TargetType"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="source">The input value.</param>
+		/// <param name="sourceType">The actual type of the source.</param>
+		/// <param name="defaultValue">The default value.</param>
+		/// <returns>Returns the converted value.</returns>
+		protected override double DoConvert(IMansionContext context, Int64 source, Type sourceType, double defaultValue)
+		{
+			return source;
+		}
+		#endregion
+	}
+}
