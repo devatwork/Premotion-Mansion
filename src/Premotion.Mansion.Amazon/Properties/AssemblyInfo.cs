82941112ad9869d309d6f7a54b0e46ad1d5bb378
Bert Willems <b.willems@premotion.nl>
2012-06-23 20:21:13 +0200

    + udpated the version number to 1.1.8

--- src/Premotion.Mansion.Amazon/Properties/AssemblyInfo.cs
+++ src/Premotion.Mansion.Amazon/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using Premotion.Mansion.Core.Nucleus;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.7")]
-[assembly: AssemblyFileVersion("1.1.7")]
+[assembly: AssemblyVersion("1.1.8")]
+[assembly: AssemblyFileVersion("1.1.8")]
 [assembly: CLSCompliant(true)]
 [assembly: ScanAssembly()]
