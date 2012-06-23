153898a0afc63b5921b8556a9e09da24997fcbb3
Bert Willems <b.willems@premotion.nl>
2012-06-23 14:12:43 +0200

    #49 Add a removeRowFromSet script tag

---
+++ src/Premotion.Mansion.Core/ScriptTags/Stack/RemoveRowFromSetTag.cs
@@ -0,0 +1,21 @@
+﻿using Premotion.Mansion.Core.Collections;
+using Premotion.Mansion.Core.Scripting.TagScript;
+
+namespace Premotion.Mansion.Core.ScriptTags.Stack
+{
+	/// <summary>
+	/// Removes an existing row from an existing dataset.
+	/// </summary>
+	[ScriptTag(Constants.NamespaceUri, "removeRowFromSet")]
+	public class RemoveRowFromSetTag : ScriptTag
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="context"></param>
+		protected override void DoExecute(IMansionContext context)
+		{
+			// add the row
+			GetRequiredAttribute<Dataset>(context, "target").RemoveRow(GetRequiredAttribute<IPropertyBag>(context, "source"));
+		}
+	}
+}
