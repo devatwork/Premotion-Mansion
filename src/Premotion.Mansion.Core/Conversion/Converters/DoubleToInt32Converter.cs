12451d59715311a63027708d7188ed79f11713b9
Bert Willems <b.willems@premotion.nl>
2012-06-24 12:17:08 +0200

    + added a convert for Double to Int conversion

---
+++ src/Premotion.Mansion.Core/Conversion/Converters/DoubleToInt32Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Premotion.Mansion.Core.Conversion.Converters
+{
+	/// <summary>
+	/// Handles the conversion from <see cref="double"/> to <see cref="int"/>.
+	/// </summary>
+	public class DoubleToInt32Converter : ConverterBase<double, Int32>
+	{
+		#region Overrides of ConverterBase<double,Int32>
+		/// <summary>
+		/// Converts the object to <see cref="IConverter.TargetType"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="source">The input value.</param>
+		/// <param name="sourceType">The actual type of the source.</param>
+		/// <returns>Returns the converted value.</returns>
+		protected override Int32 DoConvert(IMansionContext context, double source, Type sourceType)
+		{
+			return System.Convert.ToInt32(source);
+		}
+		/// <summary>
+		/// Converts the object to <see cref="IConverter.TargetType"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="source">The input value.</param>
+		/// <param name="sourceType">The actual type of the source.</param>
+		/// <param name="defaultValue">The default value.</param>
+		/// <returns>Returns the converted value.</returns>
+		protected override Int32 DoConvert(IMansionContext context, double source, Type sourceType, Int32 defaultValue)
+		{
+			return System.Convert.ToInt32(source);
+		}
+		#endregion
+	}
+}
