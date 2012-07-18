f696e199082dfd9d218ef142ab285d843bf0b1e1
Bert Willems <b.willems@premotion.nl>
2012-07-18 20:38:14 +0200

    + getPlginDataset

--- src/Premotion.Mansion.Web/Cms/Descriptors/CmsPluginDescriptor.cs
+++ src/Premotion.Mansion.Web/Cms/Descriptors/CmsPluginDescriptor.cs
@@ -1,4 +1,6 @@
-﻿using Premotion.Mansion.Core.Types;
+﻿using System;
+using Premotion.Mansion.Core;
+using Premotion.Mansion.Core.Types;
 
 namespace Premotion.Mansion.Web.Cms.Descriptors
 {
@@ -8,5 +10,20 @@ namespace Premotion.Mansion.Web.Cms.Descriptors
 	[TypeDescriptor(Constants.DescriptorNamespaceUri, "plugin")]
 	public class CmsPluginDescriptor : TypeDescriptor
 	{
+		#region Properties
+		/// <summary>
+		/// Gets the order of this plugin.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <returns>Returns the order number.</returns>
+		public int GetOrder(IMansionContext context)
+		{
+			// validate arguments
+			if (context == null)
+				throw new ArgumentNullException("context");
+
+			return Properties.Get<int>(context, "order");
+		}
+		#endregion
 	}
 }
