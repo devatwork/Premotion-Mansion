0a41474518c38f08dbc807434d7b326838687761
Bert Willems <b.willems@premotion.nl>
2012-07-18 12:10:49 +0200

    + initial checkin of the plugin structure

---
+++ src/Premotion.Mansion.Web/Cms/Descriptors/CmsPluginDescriptor.cs
@@ -0,0 +1,12 @@
+﻿using Premotion.Mansion.Core.Types;
+
+namespace Premotion.Mansion.Web.Cms.Descriptors
+{
+	/// <summary>
+	/// Describes a CMS plugin.
+	/// </summary>
+	[TypeDescriptor(Constants.DescriptorNamespaceUri, "plugin")]
+	public class CmsPluginDescriptor : TypeDescriptor
+	{
+	}
+}
