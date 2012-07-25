0b9dadcbcd6a2de2b811a012b60b439c32ba779a
Bert Willems <b.willems@premotion.nl>
2012-07-25 16:45:54 +0200

    + 1.2.1-alpha

--- src/Premotion.Mansion.Web.Portal/Properties/AssemblyInfo.cs
+++ src/Premotion.Mansion.Web.Portal/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using Premotion.Mansion.Core.Nucleus;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.2.1")]
+[assembly: AssemblyFileVersion("1.2.1")]
 [assembly: CLSCompliant(true)]
 [assembly: ScanAssembly]
