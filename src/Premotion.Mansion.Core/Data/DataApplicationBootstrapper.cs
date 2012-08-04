bc0a273c7924f9e0ac1553aee3f8510022900021
Bert Willems <b.willems@premotion.nl>
2012-08-04 20:20:49 +0200

    + auto register the query parser

---
+++ src/Premotion.Mansion.Core/Data/DataApplicationBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Premotion.Mansion.Core.Data.Parser;
+using Premotion.Mansion.Core.Nucleus;
+
+namespace Premotion.Mansion.Core.Data
+{
+	/// <summary>
+	/// Registers all the services used by repository applications.
+	/// </summary>
+	public class DataApplicationBootstrapper : ApplicationBootstrapper
+	{
+		#region Constructors
+		/// <summary>
+		/// </summary>
+		public DataApplicationBootstrapper() : base(10)
+		{
+		}
+		#endregion
+		#region Overrides of ApplicationBootstrapper
+		/// <summary>
+		/// Registers all the services used by the application in the <paramref name="nucleus"/>.
+		/// </summary>
+		/// <param name="nucleus">The <see cref="IConfigurableNucleus"/> in which to register the services used by the application.</param>
+		protected override void DoBoostrap(IConfigurableNucleus nucleus)
+		{
+			// registers the portal service
+			nucleus.Register<IQueryParser>(resolver => new QueryParser(resolver.Resolve<QueryArgumentProcessor>()));
+		}
+		#endregion
+	}
+}
