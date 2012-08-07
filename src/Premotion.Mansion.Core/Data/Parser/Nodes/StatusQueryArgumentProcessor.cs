95504dda784f6e054292cb4cd57ad1e9e3888122
Bert Willems <b.willems@premotion.nl>
2012-08-07 14:01:13 +0200

    + status specification

---
+++ src/Premotion.Mansion.Core/Data/Parser/Nodes/StatusQueryArgumentProcessor.cs
@@ -0,0 +1,33 @@
+using Premotion.Mansion.Core.Data.Specifications.Nodes;
+
+namespace Premotion.Mansion.Core.Data.Parser.Nodes
+{
+	/// <summary>
+	/// Implements the status query argument processor.
+	/// </summary>
+	public class StatusQueryArgumentProcessor : QueryArgumentProcessor
+	{
+		#region Constructors
+		/// <summary>
+		/// Constructs this processor.
+		/// </summary>
+		public StatusQueryArgumentProcessor() : base(100)
+		{
+		}
+		#endregion
+		#region Overrides of QueryArgumentProcessor
+		/// <summary>
+		/// Processes the <paramref name="parameters"/> and turn them into <paramref name="query"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="parameters">The parameters which to process.</param>
+		/// <param name="query">The <see cref="Query"/> in which to set the parameters.</param>
+		protected override void DoProcess(IMansionContext context, IPropertyBag parameters, Query query)
+		{
+			string status;
+			if (parameters.TryGetAndRemove(context, "status", out status) && !string.IsNullOrEmpty(status))
+				query.Add(StatusSpecification.Is(status));
+		}
+		#endregion
+	}
+}
