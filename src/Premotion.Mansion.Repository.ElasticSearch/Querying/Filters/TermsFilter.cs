9bdb80517bc8e00354bbbc8447c2c61d4cc0c234
Bert Willems <b.willems@premotion.nl>
2012-12-20 19:16:37 +0100

    + IsPropertyInSpecificationMapper specification

---
+++ src/Premotion.Mansion.Repository.ElasticSearch/Querying/Filters/TermsFilter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Premotion.Mansion.Repository.ElasticSearch.Querying.Filters
+{
+	/// <summary>
+	/// Implements a terms filter.
+	/// </summary>
+	[JsonConverter(typeof (TermsFilterConverter))]
+	[JsonObject(MemberSerialization.OptIn)]
+	public class TermsFilter : BaseFilter
+	{
+		#region Nested type: TermsFilterConverter
+		private class TermsFilterConverter : BaseFilterConverter<TermsFilter>
+		{
+			#region Overrides of BaseConverter<CompositeFilter>
+			/// <summary>
+			/// Writes the JSON representation of the object.
+			/// </summary>
+			/// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter"/> to write to.</param><param name="value">The value.</param><param name="serializer">The calling serializer.</param>
+			protected override void DoWriteJson(JsonWriter writer, TermsFilter value, JsonSerializer serializer)
+			{
+				// writer the inner filter
+				writer.WriteStartObject();
+				writer.WritePropertyName("terms");
+				writer.WriteStartObject();
+				writer.WritePropertyName(value.propertyName.ToLower());
+				serializer.Serialize(writer, value.values);
+				WriteObjectContent(writer, value, serializer);
+				writer.WriteEndObject();
+				writer.WriteEndObject();
+			}
+			#endregion
+		}
+		#endregion
+		#region Constructors
+		/// <summary>
+		/// Constructs the term filter.
+		/// </summary>
+		/// <param name="propertyName">The name of the property on which to filter.</param>
+		/// <param name="values">The values the property should have.</param>
+		/// <exception cref="ArgumentNullException">Thrown if one of the parameters is null.</exception>
+		public TermsFilter(string propertyName, IEnumerable<object> values)
+		{
+			// validate arguments
+			if (string.IsNullOrEmpty(propertyName))
+				throw new ArgumentNullException("propertyName");
+			if (values == null)
+				throw new ArgumentNullException("values");
+
+			// set value
+			this.propertyName = propertyName;
+			this.values = values.ToArray();
+		}
+		#endregion
+		#region Private Fields
+		private readonly string propertyName;
+		private readonly object[] values;
+		#endregion
+	}
+}
---
+++ src/Premotion.Mansion.Repository.ElasticSearch/Querying/Mappers/IsPropertyInSpecificationMapper.cs
@@ -0,0 +1,31 @@
+﻿using Premotion.Mansion.Core;
+using Premotion.Mansion.Core.Data.Queries;
+using Premotion.Mansion.Core.Data.Queries.Specifications;
+using Premotion.Mansion.Repository.ElasticSearch.Querying.Filters;
+
+namespace Premotion.Mansion.Repository.ElasticSearch.Querying.Mappers
+{
+	/// <summary>
+	/// Defines the allowed roles <see cref="BaseFilter"/>.
+	/// </summary>
+	public class IsPropertyInSpecificationMapper : BaseSpecificationMapper<IsPropertyInSpecification>
+	{
+		#region Overrides of BaseSpecificationMapper<IsPropertyInSpecification>
+		/// <summary>
+		/// Maps the given <paramref name="specification"/> to the <paramref name="searchQuery"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="query">The source <see cref="Query"/> being mapped.</param>
+		/// <param name="specification">The source <see cref="Specification"/> being mapped.</param>
+		/// <param name="searchQuery">The <see cref="SearchQuery"/> to which to map <paramref name="specification"/>.</param>
+		protected override void DoMap(IMansionContext context, Query query, IsPropertyInSpecification specification, SearchQuery searchQuery)
+		{
+			// add a term filter
+			searchQuery.Add(new TermsFilter(specification.PropertyName, specification.Values)
+			                {
+			                	Cache = false
+			                });
+		}
+		#endregion
+	}
+}
