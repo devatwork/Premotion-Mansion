9eba7801b6efe567db168f5dc5b73225ede9ebed
Bert Willems <b.willems@premotion.nl>
2012-12-20 18:45:49 +0100

    + property is specification

---
+++ src/Premotion.Mansion.Repository.ElasticSearch/Querying/Mappers/IsPropertyEqualSpecificationMapper.cs
@@ -0,0 +1,31 @@
+﻿using Premotion.Mansion.Core;
+using Premotion.Mansion.Core.Data.Queries;
+using Premotion.Mansion.Core.Data.Queries.Specifications;
+using Premotion.Mansion.Repository.ElasticSearch.Querying.Filters;
+
+namespace Premotion.Mansion.Repository.ElasticSearch.Querying.Mappers
+{
+	/// <summary>
+	/// Defines the allowed roles <see cref="BaseFilter"/>.
+	/// </summary>
+	public class IsPropertyEqualSpecificationMapper : BaseSpecificationMapper<IsPropertyEqualSpecification>
+	{
+		#region Overrides of BaseSpecificationMapper<IsPropertyEqualSpecification>
+		/// <summary>
+		/// Maps the given <paramref name="specification"/> to the <paramref name="searchQuery"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="query">The source <see cref="Query"/> being mapped.</param>
+		/// <param name="specification">The source <see cref="Specification"/> being mapped.</param>
+		/// <param name="searchQuery">The <see cref="SearchQuery"/> to which to map <paramref name="specification"/>.</param>
+		protected override void DoMap(IMansionContext context, Query query, IsPropertyEqualSpecification specification, SearchQuery searchQuery)
+		{
+			// add a term filter
+			searchQuery.Add(new TermFilter(specification.PropertyName, specification.Value)
+			                {
+			                	Cache = false
+			                });
+		}
+		#endregion
+	}
+}
