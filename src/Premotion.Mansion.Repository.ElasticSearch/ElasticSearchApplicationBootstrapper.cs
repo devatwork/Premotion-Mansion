8164a1f8e37b3c2914b4ef018e7aed5d90ce7fbb
Bert Willems <b.willems@premotion.nl>
2012-12-23 12:21:18 +0100

    + registered the ElasticSearch Index- and Query- Engine

--- src/Premotion.Mansion.Repository.ElasticSearch/ElasticSearchApplicationBootstrapper.cs
+++ src/Premotion.Mansion.Repository.ElasticSearch/ElasticSearchApplicationBootstrapper.cs
@@ -1,7 +1,9 @@
-﻿using Premotion.Mansion.Core;
+﻿using System.Configuration;
+using Premotion.Mansion.Core;
 using Premotion.Mansion.Core.Nucleus;
 using Premotion.Mansion.Core.Types;
 using Premotion.Mansion.Repository.ElasticSearch.Connection;
+using Premotion.Mansion.Repository.ElasticSearch.Data;
 using Premotion.Mansion.Repository.ElasticSearch.Indexing;
 using Premotion.Mansion.Repository.ElasticSearch.Querying;
 using Premotion.Mansion.Repository.ElasticSearch.Schema;
@@ -27,11 +29,19 @@ namespace Premotion.Mansion.Repository.ElasticSearch
 		/// <param name="nucleus">The <see cref="IConfigurableNucleus"/> in which to register the services used by the application.</param>
 		protected override void DoBootstrap(IConfigurableNucleus nucleus)
 		{
+			// if elastic search is not configured, do not register its services
+			var appSettings = ConfigurationManager.AppSettings;
+			var connectionString = appSettings[Constants.ConnectionStringApplicationSettingKey];
+			if (string.IsNullOrEmpty(connectionString))
+				return;
+
 			// register the ElasticSearch services
 			nucleus.Register(resolver => new ConnectionManager());
 			nucleus.Register(resolver => new IndexDefinitionResolver(resolver.ResolveSingle<ITypeService>()));
 			nucleus.Register(resolver => new Indexer(resolver.ResolveSingle<ConnectionManager>(), resolver.ResolveSingle<IndexDefinitionResolver>()));
 			nucleus.Register(resolver => new Searcher(resolver.ResolveSingle<ConnectionManager>(), resolver.ResolveSingle<IndexDefinitionResolver>(), resolver.Resolve<IQueryComponentMapper>()));
+			nucleus.Register(resolver => new ElasticSearchIndexEngine(resolver.ResolveSingle<Indexer>()));
+			nucleus.Register(resolver => new ElasticSearchQueryEngine(resolver.ResolveSingle<Searcher>(), resolver.ResolveSingle<IndexDefinitionResolver>()));
 		}
 		#endregion
 	}
