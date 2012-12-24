59d5f842c1ecb9e4e4757ece7138d96d5a6bc5b3
Bert Willems <b.willems@premotion.nl>
2012-12-24 13:33:42 +0100

    + property compare specification mappers

---
+++ src/Premotion.Mansion.Repository.ElasticSearch/Querying/Mappers/IsPropertyGreaterThanSpecificationMapper.cs
@@ -0,0 +1,32 @@
+using Premotion.Mansion.Core;
+using Premotion.Mansion.Core.Data.Queries;
+using Premotion.Mansion.Core.Data.Queries.Specifications;
+using Premotion.Mansion.Repository.ElasticSearch.Querying.Filters;
+using Premotion.Mansion.Repository.ElasticSearch.Schema;
+
+namespace Premotion.Mansion.Repository.ElasticSearch.Querying.Mappers
+{
+	/// <summary>
+	/// Defines the allowed roles <see cref="IsPropertyGreaterThanSpecification"/>.
+	/// </summary>
+	public class IsPropertyGreaterThanSpecificationMapper : BaseSpecificationMapper<IsPropertyGreaterThanSpecification>
+	{
+		#region Overrides of BaseSpecificationMapper<IsPropertyGreaterThanSpecification>
+		/// <summary>
+		/// Maps the given <paramref name="specification"/> to the <paramref name="searchQuery"/>.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="query">The source <see cref="Query"/> being mapped.</param>
+		/// <param name="specification">The source <see cref="Specification"/> being mapped.</param>
+		/// <param name="searchQuery">The <see cref="SearchQuery"/> to which to map <paramref name="specification"/>.</param>
+		protected override void DoMap(IMansionContext context, Query query, IsPropertyGreaterThanSpecification specification, SearchQuery searchQuery)
+		{
+			// find the property mapping
+			var propertyMapping = searchQuery.TypeMapping.FindPropertyMapping<SinglePropertyMapping>(specification.PropertyName);
+
+			// add a range filter
+			searchQuery.Add(RangeFilter.GreaterThan(specification.PropertyName, propertyMapping.Normalize(specification.Value)));
+		}
+		#endregion
+	}
+}
