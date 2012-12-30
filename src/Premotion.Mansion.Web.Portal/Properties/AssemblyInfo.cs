73f499054ee557a47f55abfd2b3f4a474feba2e0
Bert Willems <b.willems@premotion.nl>
2012-12-30 17:28:52 +0100

    + boosted version number to 1.2.19

--- src/Premotion.Mansion.Web.Portal/Properties/AssemblyInfo.cs
+++ src/Premotion.Mansion.Web.Portal/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.18")]
-[assembly: AssemblyFileVersion("1.2.18")]
+[assembly: AssemblyVersion("1.2.19")]
+[assembly: AssemblyFileVersion("1.2.19")]
