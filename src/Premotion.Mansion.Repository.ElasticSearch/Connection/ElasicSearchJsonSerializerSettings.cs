b1c137bdc66f1831583e20c316997cf19a70082b
Bert Willems <b.willems@premotion.nl>
2013-01-12 11:47:42 +0100

    + use common serializer settings

---
+++ src/Premotion.Mansion.Repository.ElasticSearch/Connection/ElasicSearchJsonSerializerSettings.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Premotion.Mansion.Repository.ElasticSearch.Responses.Facets;
+
+namespace Premotion.Mansion.Repository.ElasticSearch.Connection
+{
+	/// <summary>
+	/// Contains the settings used for the elastic search serializer.
+	/// </summary>
+	public static class ElasicSearchJsonSerializerSettings
+	{
+		/// <summary>
+		/// The <see cref="JsonSerializerSettings"/> used for elastic search.
+		/// </summary>
+		public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+		                                                         {
+		                                                         	MissingMemberHandling = MissingMemberHandling.Ignore,
+		                                                         	NullValueHandling = NullValueHandling.Ignore,
+		                                                         	DefaultValueHandling = DefaultValueHandling.Include,
+		                                                         	DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+		                                                         	Converters = new List<JsonConverter>
+		                                                         	             {
+		                                                         	             	new Facet.FacetReadConverter()
+		                                                         	             }
+		                                                         };
+	}
+}
