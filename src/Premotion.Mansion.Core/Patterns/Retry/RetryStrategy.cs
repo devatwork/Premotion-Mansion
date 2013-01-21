58482d50e15827b910d9b1820a9c46a56ecdb9f8
Bert Willems <b.willems@premotion.nl>
2013-01-21 12:17:51 +0100

    + retry

---
+++ src/Premotion.Mansion.Core/Patterns/Retry/RetryStrategy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Premotion.Mansion.Core.Patterns.Retry
+{
+	/// <summary>
+	/// Represents a retry strategy.
+	/// </summary>
+	public abstract class RetryStrategy
+	{
+		/// <summary>
+		/// Executes the given <paramref name="action"/> using this retry strategy.
+		/// </summary>
+		/// <param name="action">The <see cref="Action"/> which to execute.</param>
+		public void Execute(Action action)
+		{
+			// validate arguments
+			if (action == null)
+				throw new ArgumentNullException("action");
+
+			//  invoke the template method
+			DoExecute(action);
+		}
+		/// <summary>
+		/// Executes the given <paramref name="action"/> using this retry strategy.
+		/// </summary>
+		/// <param name="action">The <see cref="Action"/> which to execute.</param>
+		protected abstract void DoExecute(Action action);
+	}
+}
