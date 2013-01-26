9468e11d7a7e72bcf2554edc3ce7eedbbf3e4389
Bert Willems <b.willems@premotion.nl>
2013-01-26 13:59:39 +0100

    + csv reader tag

--- src/Premotion.Mansion.Core/IO/Csv/CsvFormat.cs
+++ src/Premotion.Mansion.Core/IO/Csv/CsvFormat.cs
@@ -1,4 +1,7 @@
-﻿namespace Premotion.Mansion.Core.IO.Csv
+﻿using System;
+using System.Collections.Generic;
+
+namespace Premotion.Mansion.Core.IO.Csv
 {
 	/// <summary>
 	/// Represents the format of an CSV document.
@@ -30,6 +33,31 @@
 			RowDelimitor = "\r\n",
 			TextQualifier = "\""
 		};
+		private static readonly IDictionary<string, CsvFormat> Formats = new Dictionary<string, CsvFormat>(StringComparer.OrdinalIgnoreCase) {
+			{"DutchExcel", DutchExcel},
+			{"EnglishExcel", EnglishExcel},
+			{"Tab", Tab}
+		};
+		#endregion
+		#region Methods
+		/// <summary>
+		/// Gets a <see cref="CsvFormat"/> by it's <paramref name="formatName"/>.
+		/// </summary>
+		/// <param name="formatName">The name of the format which to get.</param>
+		/// <returns>Returns the <see cref="CsvFormat"/> matching the name.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="formatName"/> is null  or empty.</exception>
+		/// <exception cref="InvalidOperationException">Thrown if no format with <paramref name="formatName"/> was found.</exception>
+		public static CsvFormat GetByName(string formatName)
+		{
+			// validate arguments
+			if (string.IsNullOrEmpty(formatName))
+				throw new ArgumentNullException("formatName");
+
+			CsvFormat format;
+			if (!Formats.TryGetValue(formatName, out format))
+				throw new InvalidOperationException(string.Format("Could not find CSV format for name '{0}'", formatName));
+			return format;
+		}
 		#endregion
 		#region Properties
 		/// <summary>
---
+++ src/Premotion.Mansion.Core/ScriptTags/Media/Csv/GetCsvReaderTag.cs
@@ -0,0 +1,35 @@
+﻿using Premotion.Mansion.Core.IO.Csv;
+using Premotion.Mansion.Core.Scripting.TagScript;
+
+namespace Premotion.Mansion.Core.ScriptTags.Media.Csv
+{
+	/// <summary>
+	/// Gets a CSV reader to read CSV data from the underlying pipe.
+	/// </summary>
+	[ScriptTag(Constants.NamespaceUri, "getCsvReader")]
+	public class GetCsvReaderTag : ScriptTag
+	{
+		#region Overrides of ScriptTag
+		/// <summary>
+		/// Executes this tag.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		protected override void DoExecute(IMansionContext context)
+		{
+			// get the input pipe
+			var inputPipe = context.InputPipe;
+
+			// parse the format
+			var formatName = GetRequiredAttribute<string>(context, "format");
+			var format = CsvFormat.GetByName(formatName);
+
+			// create the reader
+			var reader = new CsvReader(inputPipe, format);
+
+			// push the reader to the stack
+			using (context.ReaderStack.Push(reader))
+				ExecuteChildTags(context);
+		}
+		#endregion
+	}
+}
