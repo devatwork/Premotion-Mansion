6947cbe503f591a88912b4bde17c8db82890f97c
Bert Willems <b.willems@premotion.nl>
2013-04-13 10:46:07 +0200

    + linkbase descriptor

---
+++ src/Premotion.Mansion.Linking/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Premotion.Mansion.Linking
+{
+	/// <summary>
+	/// Represents constants.
+	/// </summary>
+	public static class Constants
+	{
+		/// <summary>
+		/// The namespace in which the Linking descriptors live.
+		/// </summary>
+		public const string DescriptorNamespaceUri = @"http://schemas.premotion.nl/mansion/1.0/link-descriptors.xsd";
+	}
+}
---
+++ src/Premotion.Mansion.Linking/Descriptors/LinkbaseDescriptor.cs
@@ -0,0 +1,12 @@
+﻿using Premotion.Mansion.Core.Types;
+
+namespace Premotion.Mansion.Linking.Descriptors
+{
+	/// <summary>
+	/// This type has a linkbase, indicating that rich links can be made from and to this type.
+	/// </summary>
+	[TypeDescriptor(Constants.DescriptorNamespaceUri, "linkbase")]
+	public class LinkbaseDescriptor : TypeDescriptor
+	{
+	}
+}
