0787cf57a9f9d14ae7069205aa7daa028eb4f16e
Bert Willems <b.willems@premotion.nl>
2013-04-13 08:39:18 +0200

    + knowledge system project

--- src/package.cmd
+++ src/package.cmd
@@ -3,6 +3,7 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Premotion.Mansion.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 .nuget\NuGet.exe pack Premotion.Mansion.Amazon\Premotion.Mansion.Amazon.nuspec
 .nuget\NuGet.exe pack Premotion.Mansion.Core\Premotion.Mansion.Core.nuspec
+.nuget\NuGet.exe pack Premotion.Mansion.KnowledgeOrganization\Premotion.Mansion.KnowledgeOrganization.nuspec
 .nuget\NuGet.exe pack Premotion.Mansion.Repository.ElasticSearch\Premotion.Mansion.Repository.ElasticSearch.nuspec
 .nuget\NuGet.exe pack Premotion.Mansion.Repository.SqlServer\Premotion.Mansion.Repository.SqlServer.nuspec
 .nuget\NuGet.exe pack Premotion.Mansion.Web\Premotion.Mansion.Web.nuspec
@@ -12,6 +13,7 @@
 # %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Premotion.Mansion.sln /t:Clean,Rebuild /p:Configuration=Debug /fileLogger
 # .nuget\NuGet.exe pack Premotion.Mansion.Amazon\Premotion.Mansion.Amazon.symbols.nuspec -Symbols
 # .nuget\NuGet.exe pack Premotion.Mansion.Core\Premotion.Mansion.Core.symbols.nuspec -Symbols
+# .nuget\NuGet.exe pack Premotion.Mansion.KnowledgeOrganization\Premotion.Mansion.KnowledgeOrganization.symbols.nuspec -Symbols
 # .nuget\NuGet.exe pack Premotion.Mansion.Repository.ElasticSearch\Premotion.Mansion.Repository.ElasticSearch.symbols.nuspec -Symbols
 # .nuget\NuGet.exe pack Premotion.Mansion.Repository.SqlServer\Premotion.Mansion.Repository.SqlServer.symbols.nuspec -Symbols
 # .nuget\NuGet.exe pack Premotion.Mansion.Web\Premotion.Mansion.Web.symbols.nuspec -Symbols
