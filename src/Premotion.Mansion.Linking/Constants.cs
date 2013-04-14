d055329703bf2fb41ec948ba0577dfd00ccbd3bf
Bert Willems <b.willems@premotion.nl>
2013-04-14 09:58:40 +0200

    + get incoming link dataset tag

--- src/Premotion.Mansion.Linking/Constants.cs
+++ src/Premotion.Mansion.Linking/Constants.cs
@@ -13,5 +13,9 @@
 		/// The namespace in which the Linking descriptors live.
 		/// </summary>
 		public const string DescriptorNamespaceUri = @"http://schemas.premotion.nl/mansion/1.0/link-descriptors.xsd";
+		/// <summary>
+		/// The namespace in which the Linking tags live.
+		/// </summary>
+		public const string TagNamespaceUri = @"http://schemas.premotion.nl/mansion/1.0/link-tags.xsd";
 	}
 }
