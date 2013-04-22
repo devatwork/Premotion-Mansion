ae6f6d490c9e9968656ba9e2e0ed852c3ba80794
Bert Willems <b.willems@premotion.nl>
2013-04-22 20:46:40 +0200

    + boosted version number

--- src/Premotion.Mansion.Core/Properties/AssemblyInfo.cs
+++ src/Premotion.Mansion.Core/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.30")]
-[assembly: AssemblyFileVersion("1.2.30")]
+[assembly: AssemblyVersion("1.2.31")]
+[assembly: AssemblyFileVersion("1.2.31")]
 [assembly: CLSCompliant(true)]
