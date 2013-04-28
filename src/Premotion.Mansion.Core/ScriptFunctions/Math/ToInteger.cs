e9ae57b6e0e5a6d64138651e92a629b725b9c777
Bert Willems <b.willems@premotion.nl>
2013-04-28 10:47:30 +0200

    + ToInteger script function

---
+++ src/Premotion.Mansion.Core/ScriptFunctions/Math/ToInteger.cs
@@ -0,0 +1,21 @@
+﻿using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Core.ScriptFunctions.Math
+{
+	/// <summary>
+	/// Converts the input to an integer.
+	/// </summary>
+	[ScriptFunction("ToInteger")]
+	public class ToInteger : FunctionExpression
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public int Evaluate(IMansionContext context, int input)
+		{
+			return input;
+		}
+	}
+}
