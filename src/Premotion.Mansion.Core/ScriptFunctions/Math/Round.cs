d358e0095c0d3a854e1c448dc86ec0aee14eeecd
Bert Willems <b.willems@premotion.nl>
2013-04-28 10:41:25 +0200

    + round, ceiling & floor script function

---
+++ src/Premotion.Mansion.Core/ScriptFunctions/Math/Round.cs
@@ -0,0 +1,21 @@
+﻿using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Core.ScriptFunctions.Math
+{
+	/// <summary>
+	/// Rounds a double-precision floating-point value to the nearest integral value.
+	/// </summary>
+	[ScriptFunction("Round")]
+	public class Round : FunctionExpression
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public int Evaluate(IMansionContext context, double value)
+		{
+			return (int) System.Math.Round(value);
+		}
+	}
+}
