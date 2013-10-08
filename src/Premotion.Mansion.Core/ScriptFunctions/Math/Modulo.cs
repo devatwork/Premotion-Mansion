9d6a40a2cf4501be9e04af67a6ed67ea78b77a2d
Erikvl87 <erikvl87@gmail.com>
2013-10-08 22:56:47 +0200

    Added modulo math scriptfunction

---
+++ src/Premotion.Mansion.Core/ScriptFunctions/Math/Modulo.cs
@@ -0,0 +1,22 @@
+﻿using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Core.ScriptFunctions.Math
+{
+	/// <summary>
+	/// Multiplies the first value with the second value.
+	/// </summary>
+	[ScriptFunction("Modulo")]
+	public class Modulo : FunctionExpression
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="first"></param>
+		/// <param name="second"></param>
+		/// <returns></returns>
+		public double Evaluate(IMansionContext context, double first, double second)
+		{
+			return first % second;
+		}
+	}
+}
