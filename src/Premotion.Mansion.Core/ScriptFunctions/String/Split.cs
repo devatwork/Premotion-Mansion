c5d44477a10c80cfa7e0c5695347a92b3efe6a4b
Bert Willems <b.willems@premotion.nl>
2013-10-11 15:39:51 +0200

    added the split script function

---
+++ src/Premotion.Mansion.Core/ScriptFunctions/String/Split.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Core.ScriptFunctions.String
+{
+	/// <summary>
+	/// Splits a given string.
+	/// </summary>
+	[ScriptFunction("Split")]
+	public class Split : FunctionExpression
+	{
+		/// <summary>
+		/// Splits a given string.
+		/// </summary>
+		/// <param name="context">The <see cref="IMansionContext"/>.</param>
+		/// <param name="input">The input which to split.</param>
+		/// <returns>Returns the splitted parts.</returns>
+		public string[] Evaluate(IMansionContext context, string input)
+		{
+			// validate arguments
+			if (context == null)
+				throw new ArgumentNullException("context");
+
+			return Evaluate(context, input, ",");
+		}
+		/// <summary>
+		/// Splits a given string.
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="input">The input which to split.</param>
+		/// <param name="separator">The seperator which to use.</param>
+		/// <returns>Returns the splitted parts.</returns>
+		public string[] Evaluate(IMansionContext context, string input, string separator)
+		{
+			// validate arguments
+			if (context == null)
+				throw new ArgumentNullException("context");
+
+			return (input ?? string.Empty).Split(new[] {separator}, StringSplitOptions.RemoveEmptyEntries);
+		}
+	}
+}
