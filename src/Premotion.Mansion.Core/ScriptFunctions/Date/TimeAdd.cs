bef47a73dacd4d849408cad9e9d8f8281b231065
Erik van Leeuwen <erikvl87@gmail.com>
2014-05-24 14:49:45 +0200

    Add ScriptFunction TimeAdd.

---
+++ src/Premotion.Mansion.Core/ScriptFunctions/Date/TimeAdd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Core.ScriptFunctions.Date
+{
+	/// <summary>
+	/// Adds a number of days to the date.
+	/// </summary>
+	[ScriptFunction("TimeAdd")]
+	public class TimeAdd : FunctionExpression
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		/// <param name="input"></param>
+		/// <param name="hours"></param>
+		/// <param name="minutes"></param>
+		/// <param name="seconds"></param>
+		public DateTime Evaluate(IMansionContext context, DateTime input, int hours, int minutes, int seconds)
+		{
+			// validate arguments
+			if (context == null)
+				throw new ArgumentNullException("context");
+			if (input == DateTime.MinValue)
+				return DateTime.MinValue;
+
+			return input.AddHours(hours).AddMinutes(minutes).AddSeconds(seconds);
+		}
+	}
+}
