be83e633afb39f42635fc3fa8296af6e32bb4f22
Erik van Leeuwen <erikvl87@gmail.com>
2014-08-16 16:37:45 +0200

    Add script function FormatTimeSpan

---
+++ src/Premotion.Mansion.Core/ScriptFunctions/Date/FormatTimeSpan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Core.ScriptFunctions.Date
+{
+	/// <summary>
+	/// Implements the format date function.
+	/// </summary>
+	[ScriptFunction("FormatTimeSpan")]
+	public class FormatTimeSpan : FunctionExpression
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="time"></param>
+		/// <param name="format"></param>
+		/// <returns></returns>
+		public string Evaluate(IMansionContext context, TimeSpan time, string format)
+		{
+			// validate arguments
+			if (time == TimeSpan.MinValue)
+				return string.Empty;
+
+			// return the formated date
+			return time.ToString(format, context.UserInterfaceCulture);
+		}
+	}
+}
