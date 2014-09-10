276e26e6ed2748478bd20a75909455dd526ab404
Erik van Leeuwen <erikvl87@gmail.com>
2014-09-10 19:32:56 +0200

    Add ScriptFunction Ticks to return the number of ticks that represent the value of the current TimeSpan structure.

---
+++ src/Premotion.Mansion.Core/ScriptFunctions/Date/Ticks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Core.ScriptFunctions.Date
+{
+	/// <summary>
+	/// Gets the number of ticks that represent the value of the current TimeSpan structure.
+	/// </summary>
+	[ScriptFunction("Ticks")]
+	public class Ticks : FunctionExpression
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		/// <param name="input"></param>
+		public long Evaluate(IMansionContext context, DateTime input)
+		{
+			// validate arguments
+			if (context == null)
+				throw new ArgumentNullException("context");
+			if (input == DateTime.MinValue)
+				return DateTime.MinValue.Ticks;
+
+			return input.Ticks;
+		}
+	}
+}
