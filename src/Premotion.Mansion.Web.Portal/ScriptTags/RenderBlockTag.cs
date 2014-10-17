8c74df7a2bba3c2ee924c6a9a33a669f97c06998
Erik van Leeuwen <erikvl87@gmail.com>
2014-10-17 21:30:11 +0200

    Add ScriptTag RenderBlock.

---
+++ src/Premotion.Mansion.Web.Portal/ScriptTags/RenderBlockTag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Premotion.Mansion.Core;
+using Premotion.Mansion.Core.Scripting.TagScript;
+using Premotion.Mansion.Web.Portal.Service;
+
+namespace Premotion.Mansion.Web.Portal.ScriptTags
+{
+	/// <summary>
+	/// Renders the specified block.
+	/// </summary>
+	[ScriptTag(Constants.TagNamespaceUri, "renderBlock")]
+	public class RenderBlockTag : ScriptTag
+	{
+		#region Constructors
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="context"></param>
+		protected override void DoExecute(IMansionContext context)
+		{
+			if (context == null)
+				throw new ArgumentNullException("context");
+
+			var blockProperties = GetAttributes(context);
+			string targetField;
+			if (!blockProperties.TryGetAndRemove(context, "targetField", out targetField) || string.IsNullOrEmpty(targetField))
+				throw new InvalidOperationException("The target attribute is manditory");
+
+			var portalService = context.Nucleus.ResolveSingle<IPortalService>();
+			portalService.RenderBlockToOutput(context, blockProperties, targetField);
+		}
+		#endregion
+	}
+}
