30c4eddb6a0423b6ebeccd3f01f723b1dcdc1f4b
Erik van Leeuwen <erikvl87@gmail.com>
2014-11-19 21:06:34 +0100

    Add a scriptfunction to escape apostrophes.

---
+++ src/Premotion.Mansion.Web/ScriptFunctions/EscapeApos.cs
@@ -0,0 +1,23 @@
+﻿using Premotion.Mansion.Core;
+using Premotion.Mansion.Core.Scripting.ExpressionScript;
+
+namespace Premotion.Mansion.Web.ScriptFunctions
+{
+	/// <summary>
+	/// Returns an apostrophe escaped string
+	/// </summary>
+	[ScriptFunction("EscapeApos")]
+	public class EscapeApos : FunctionExpression
+	{
+		/// <summary>
+		/// Returns an apostrophe escaped string
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public string Evaluate(IMansionContext context, string input)
+		{
+			return input.Replace("'", "\\'");
+		}
+	}
+}
